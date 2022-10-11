ae7d3161283cf36d84005a4a6a08f1e506239305
kharileigh <kharileighmiller@gmail.com>
2022-10-11 17:25:39 +0100

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/kharileigh/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ *@author Khari - Leigh Miller
+ *email: kharileighmiller@gmail.com
+ *date: 11.Oct.2022
+ *purpose: My first Hello World program in an IDE
+ */
+
+package com.kharileigh.helloworld;
+
+/**
+ *
+ * @author kharileigh
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Elizabeth!");
+    }
+}
