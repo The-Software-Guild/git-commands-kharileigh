c785f961948aca282b60f7f8a89b1f370009683f
kharileigh <kharileighmiller@gmail.com>
2022-10-11 17:58:34 +0100

    Mathematical expressions practice in NetBeans

--- HelloWorld/src/main/java/com/kharileigh/helloworld/HelloWorld.java
+++ HelloWorld/src/main/java/com/kharileigh/helloworld/HelloWorld.java
@@ -15,5 +15,35 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, Elizabeth!");
+        
+        // Declare variables
+        int result;
+        int operand1;
+        int operand2;
+        int operand3;
+        
+        // Initialize variables
+        result = 0;
+        operand1 = 5;
+        operand2 = 7;
+        operand3 = operand2;
+        
+        // Addition, equals 12
+        result = operand1 + operand2;
+        
+        // Subtraction, equals 3
+        result = 15 - operand1;
+        
+        // Multiplication, equals 35
+        result = operand1 * operand2;
+        
+        // Division, equals 4
+        result = 28 / 7;
+        
+        // Modulo, equals 2 remainder
+        result = operand2 % operand1;
+        
+        System.out.println("The program runs succcessfull!");
+
     }
 }
